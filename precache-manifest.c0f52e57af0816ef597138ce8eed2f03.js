55ddd716501ae5f9b313f56d13777f720a82f395
tomby5475 <pushkareva.nataly@gmail.com>
2025-09-24 13:35:38 +0200

    Updates

---
+++ precache-manifest.c0f52e57af0816ef597138ce8eed2f03.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "a0b6c0a1ac53b93b883b",
+    "url": "/beer_app/static/css/main.edc77bb9.chunk.css"
+  },
+  {
+    "revision": "a0b6c0a1ac53b93b883b",
+    "url": "/beer_app/static/js/main.45f25694.chunk.js"
+  },
+  {
+    "revision": "6e6ba57482e0d2951a62",
+    "url": "/beer_app/static/js/runtime~main.9244eefe.js"
+  },
+  {
+    "revision": "3c349bc66b6850110a8e",
+    "url": "/beer_app/static/js/2.3d3af59a.chunk.js"
+  },
+  {
+    "revision": "77da778a10b57eb72fd7",
+    "url": "/beer_app/static/js/3.5d65379c.chunk.js"
+  },
+  {
+    "revision": "72932514c26b46d02ae87e6bc10c7eab",
+    "url": "/beer_app/index.html"
+  }
+];
