2db41435136f960608d5be2c8105d20814f05bce
tomby5475 <pushkareva.nataly@gmail.com>
2025-10-08 13:13:36 +0200

    fix the link

--- src/context.jsx
+++ src/context.jsx
@@ -3,9 +3,9 @@ import axios from 'axios'
 
 const AppContext = React.createContext()
 
-const allBeers = 'https://api.punkapi.com/v3/beers'
-const randomBeer = 'https://api.punkapi.com/v3/beers/random'
-const searchBeer = 'https://api.punkapi.com/v3/beers?'
+const allBeers = 'https://punkapi.online/v3/beers'
+const randomBeer = 'https://punkapi.online/v3/beers/random'
+const searchBeer = 'https://punkapi.online/v3/beers?'
 
 const getFavoritesFromLocalStorage = () => {
   let favorites = localStorage.getItem('favorites')
